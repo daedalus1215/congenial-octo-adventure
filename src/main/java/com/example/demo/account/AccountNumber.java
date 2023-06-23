0c92dccc95532c49b1414c137024e9c831a24ef2
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-22 22:19:24 -0400

    Add builder for account number

--- src/main/java/com/example/demo/account/AccountNumber.java
+++ src/main/java/com/example/demo/account/AccountNumber.java
@@ -1,5 +1,8 @@
 package com.example.demo.account;
 
+import lombok.Builder;
+
+@Builder
 public class AccountNumber {
     private final String number;
 
@@ -7,7 +10,24 @@ public class AccountNumber {
         this.number = number;
     }
 
+    private AccountNumber(Builder builder) {
+        number = builder.number;
+    }
+
     public String getNumber() {
         return number;
+    }
+
+
+    public static final class Builder {
+        private final String number;
+
+        public Builder(String number) {
+            this.number = number;
+        }
+
+        public AccountNumber build() {
+            return new AccountNumber(this);
+        }
     }
 }
